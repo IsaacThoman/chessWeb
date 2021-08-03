267cb896aed6393fd4d9ec4613ea1d8914f03574
isaacthoman <isaacthoman@outlook.com>
2021-08-02 18:48:14 -0700

    queen rules

--- main.js
+++ main.js
@@ -356,8 +356,68 @@ return true;
         return true;
     }
 
+    if(internalBoard[source]==5){//queen
+
+        var rookLegal = false;
+        var rookLegalSet = false;
+        var bishopLegal=false;
+        var bishopLegalSet = false;
+        //start rook code
+        var scanDirX=0;
+        var scanDirY=0;
+        if (sourceX > destX){scanDirX = 0 - 1;}
+        if(sourceX < destX){scanDirX = 1;}
+        if(sourceX == destX){scanDirX = 0;}
+        if (sourceY > destY){scanDirY = 0 - 1;}
+        if(sourceY < destY){scanDirY = 1;}
+        if(sourceY == destY){scanDirY = 0;}
+        if(scanDirX!=0 &&scanDirY!=0){
+            if(!rookLegalSet){rookLegal=false;rookLegalSet=true;}
+        }
+
+        var distance = 0;
+        if(destY!=sourceY){
+            distance=Math.abs(sourceY-destY);}
+            else{
+                distance=Math.abs(sourceX-destX);
+            }
+        
+        for(var i=1;i<=distance-1;i+=1){
+            var posX = sourceX + (i*scanDirX);
+            var posY = sourceY + (i*scanDirY);
+            if(getPiece(posX,posY,internalBoard)!=0){
+                
+            
+                if(!rookLegalSet){rookLegal=false;rookLegalSet=true;}}
+            
+        }
+        if(!rookLegalSet){rookLegal=true;rookLegalSet=true;}
+        //bishop start
+        if(Math.abs(sourceX-destX) != Math.abs(sourceY - destY)){if(!bishopLegalSet){bishopLegal=false;bishopLegalSet=true;}} //fails if not perfectly diagonal
+        
+        if(getPiece(destX,destY,internalBoard)==0 | getPiece(destX, destY,internalBoard) > 6){
+             scanDirX=0;
+             scanDirY=0;
+            if (sourceX > destX){scanDirX = 0 - 1;}
+            else{scanDirX = 1;}
+            if (sourceY > destY){scanDirY = 0 - 1;}
+            else{scanDirY = 1;}
+    
+             distance = Math.abs(sourceX-destX);
+    
+            for(var i=1;i<=distance-1;i+=1){
+                var posX = sourceX + (i*scanDirX);
+                var posY = sourceY + (i*scanDirY);
+                if(getPiece(posX,posY,internalBoard)!=0){if(!bishopLegalSet){bishopLegal=false;bishopLegalSet=true;}}
+            }
+            if(!bishopLegalSet){bishopLegal=true;bishopLegalSet=true;}
+        }
+
+        return (bishopLegal||rookLegal);
+    }
+
     if(internalBoard[source]==3){//knight
-        if((Math.abs(sourceX-destX)==2&&Math.abs(sourceY-destY)==1)||(Math.abs(sourceX-destX)==1&&Math.abs(sourceY-destY)==2)){return true;}return false;
+        if((Math.abs(sourceX-destX)==2&&Math.abs(sourceY-destY)==1)||(Math.abs(sourceX-destX)==1&&Math.abs(sourceY-destY)==2)){if(!bishopLegalSet){bishopLegal=true;bishopLegalSet=true;}}if(!bishopLegalSet){bishopLegal=false;bishopLegalSet=true;};
     }
 
 
