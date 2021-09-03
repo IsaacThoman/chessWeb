00e0001ddda7943aeeb3a18bfaae29d5690ad723
Isaac Thoman <isaacthoman@outlook.com>
2021-09-03 14:52:57 -0400

    a lil bit of math

--- sidebar.js
+++ sidebar.js
@@ -3,4 +3,11 @@ function updateSidebar(){
   const ctx2 = canvas2.getContext("2d");
   var sidebarResolution = canvas2.width;
   ctx2.drawImage(pieces[1],0, 0,96, 768);
-}
+  var foundPieces = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+    for(var i=0;i<=63;i++){
+      foundPieces[boardSquares[i]]+=1;
+      }
+      console.log(foundPieces)
+
+
+    }
