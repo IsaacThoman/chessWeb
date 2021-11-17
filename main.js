4304435b3bba86b174f32f7c605f930506e22315
Isaac Thoman <isaacthoman@outlook.com>
2021-11-17 16:00:19 -0500

    spaghetti

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function showJoinBox(showHide){
 function joinBtn(){
     showJoinBox(!joinBoxElement.classList.contains("is-hidden"));
 }
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+var boardResolution = canvas.width;
+ctx.beginPath();
+ctx.rect(0, 0, 1000, 1000);
+ctx.fillStyle = "#8ca2ad";
+
+ctx.fill();
+ctx.closePath();
